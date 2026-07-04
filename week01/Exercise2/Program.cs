a7d1e5c129b5908a015bb184c4b2ef5c9686b82c
ted-kev-dorg <kevin.tetede@gmail.com>
2026-07-04 05:01:35 -0400

    W01 assignement 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+        Console.Write("What is your score? ");
+        string score = Console.ReadLine();
+
+        if (int.Parse(score) >= 90)
+        {
+            letter ="A";
+            
+        }
+
+        else if (int.Parse(score) >= 80)
+        {
+            letter ="B";
+           
+        }
+
+        else if (int.Parse(score) >= 70)
+        {
+            letter = "C";
+            
+        }
+
+        else if (int.Parse(score) >= 60)
+        {
+            letter = "D";
+            
+        }
+
+        else
+        {
+            letter ="F";
+            
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (int.Parse(score) >= 70)
+        {
+            Console.WriteLine("Congratulation! you have passed the exam");
+        }
+
+        else
+        {
+            Console.WriteLine ("Sorry, you have failed! Try again.");
+        }
+
+
+
     }
 }
