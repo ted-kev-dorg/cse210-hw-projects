34755abfe6999fba853f6f19714f0c20024ac100
ted-kev-dorg <kevin.tetede@gmail.com>
2026-07-05 00:29:19 -0400

    W01 Assignement 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next (1,100);
+
+        // Console.Write("What is the magic number? ");
+        // string choice = Console.ReadLine();
+        // int magicNumber = int.Parse(choice);
+       
+
+        int guessNumber =-1;
+        int NumberOfGuess = 0;
+
+        while (magicNumber != guessNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+            NumberOfGuess= NumberOfGuess + 1;
+
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if  (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+        }        
+        
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"You got it in {NumberOfGuess} guesses!");
+        
+
+        
     }
 }
