b50bc206afc1882addacec3b83058d9ccfea2f77
ted-kev-dorg <kevin.tetede@gmail.com>
2026-08-08 04:18:35 -0400

    W06 Shape learning assisngment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        // Test Square
+        Square square = new Square("Red", 4);
+        Console.WriteLine($"{square.GetColor()} square area: {square.GetArea()}");
+
+        // Test Rectangle
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Console.WriteLine($"{rectangle.GetColor()} rectangle area: {rectangle.GetArea()}");
+
+        // Test Circle
+        Circle circle = new Circle("Green", 2);
+        Console.WriteLine($"{circle.GetColor()} circle area: {circle.GetArea()}");
+
+        // Build a list of shapes (polymorphism in action)
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
